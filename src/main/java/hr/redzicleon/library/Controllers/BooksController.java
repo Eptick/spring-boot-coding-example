1f78b17e3dfc8228af8d03b7fb62d27f82ffa1de
Leon Redžić <redzicleon@gmail.com>
2022-02-22 23:35:38 +0100

    support bookCount property paging and sorting

--- src/main/java/hr/redzicleon/library/Controllers/BooksController.java
+++ src/main/java/hr/redzicleon/library/Controllers/BooksController.java
@@ -4,13 +4,16 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hr.redzicleon.library.domain.Author;
 import hr.redzicleon.library.domain.Book;
+import hr.redzicleon.library.repository.AuthorsRepository;
 import hr.redzicleon.library.repository.BooksRepository;
 
 @RestController()
@@ -18,6 +21,8 @@ import hr.redzicleon.library.repository.BooksRepository;
 public class BooksController {
     @Autowired
     BooksRepository booksRepository;
+    @Autowired
+    AuthorsRepository authorsRepository;
 
     @GetMapping("")
     public Page<Book> index(Pageable pageable) {
@@ -30,6 +35,12 @@ public class BooksController {
         book.setTitle(UUID.randomUUID().toString());
         book.setGenre(UUID.randomUUID().toString());
         book.setISBN("99921-58-10-7");
-        return booksRepository.save(book);
+        book = booksRepository.save(book);
+        Author author = authorsRepository.findAll(PageRequest.of(0, 5)).getContent().get(0);
+        var list = author.getBooks();
+        list.add(book);
+        author.setBooks(list);
+        authorsRepository.save(author);
+        return book;
     }
 }
--- src/main/java/hr/redzicleon/library/domain/Author.java
+++ src/main/java/hr/redzicleon/library/domain/Author.java
@@ -1,5 +1,7 @@
 package hr.redzicleon.library.domain;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -7,7 +9,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.Formula;
+
+import javax.persistence.JoinColumn;
 
 import hr.redzicleon.library.domain.audit.AuditableEntity;
 
@@ -24,6 +34,20 @@ public class Author extends AuditableEntity {
     @Column(name = "name", updatable = true, nullable = false)
     private String name;
 
+    @ManyToMany()
+    @JsonIgnore
+    @JoinTable(name = "authors_books",
+        joinColumns = @JoinColumn(name = "author_id"),
+        inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private Set<Book> books = new HashSet<>();
+
+    @Formula("(SELECT COUNT(authors_books.author_id) FROM authors " +
+         "left join authors_books on authors_books.author_id = authors.author_id " +
+         "WHERE authors.author_id = author_id " +
+         "GROUP BY authors.author_id)")
+    private int bookCount;
+
     public UUID getAuthorId() {
         return this.authorId;
     }
@@ -38,6 +62,22 @@ public class Author extends AuditableEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return this.books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public int getBookCount() {
+        return this.bookCount;
+    }
+
+    public void setBookCount(int bookCount) {
+        this.bookCount = bookCount;
     }
 
 }
