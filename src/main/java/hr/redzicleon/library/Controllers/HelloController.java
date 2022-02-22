534036b99d43f205b07ee174d42937f8f5d2fc00
Leon Redžić <redzicleon@gmail.com>
2022-02-22 15:23:16 +0100

    Rename the controller folder

--- src/main/java/hr/redzicleon/library/Controllers/HelloController.java
+++ src/main/java/hr/redzicleon/library/Controllers/HelloController.java
@@ -1,4 +1,4 @@
-package hr.redzicleon.library.Controllers;
+package hr.redzicleon.library.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
