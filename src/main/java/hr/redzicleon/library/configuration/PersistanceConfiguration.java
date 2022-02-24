8b188a0676670533edf255f41d0b8f341ba2f803
Leon Redžić <redzicleon@gmail.com>
2022-02-24 18:51:44 +0100

    @EnableTransactionManagement

--- src/main/java/hr/redzicleon/library/configuration/PersistanceConfiguration.java
+++ src/main/java/hr/redzicleon/library/configuration/PersistanceConfiguration.java
@@ -2,7 +2,9 @@ package hr.redzicleon.library.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
+@EnableTransactionManagement
 public class PersistanceConfiguration { }
