28db43c85cacdfd9763fe9e3187ccc1cb97be9e5
Leon Redžić <redzicleon@gmail.com>
2022-02-25 13:18:01 +0100

    Modify the configuration

--- src/main/java/hr/redzicleon/library/security/JWTConfig.java
+++ src/main/java/hr/redzicleon/library/security/JWTConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 
 @Configuration
+@EnableWebSecurity()
 public class JWTConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -26,17 +28,17 @@ public class JWTConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public RoleHierarchyImpl roleHierarchy() {
         RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        roleHierarchy.setHierarchy("ROLE_admin > ROLE_publisher");
+        roleHierarchy.setHierarchy("ROLE_admin > ROLE_author");
         return roleHierarchy;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authz -> authz
-                .antMatchers(HttpMethod.GET, "/authors").permitAll()
-                .antMatchers(HttpMethod.GET, "/books").permitAll()
-                .antMatchers("/authors**").hasRole("admin")
-                .antMatchers("/books**").hasRole("author")
+                .antMatchers(HttpMethod.GET, "/authors/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/books/**").permitAll()
+                .antMatchers("/authors/**").hasRole("admin")
+                .antMatchers("/books/**").hasRole("author")
                 .anyRequest().authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 	}
