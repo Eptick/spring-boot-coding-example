2df9b4489e9763529a50006ae9c526696d06f820
Leon Redžić <redzicleon@gmail.com>
2022-02-25 10:25:35 +0100

    BookController

--- src/main/java/hr/redzicleon/library/Controllers/BooksController.java
+++ src/main/java/hr/redzicleon/library/Controllers/BooksController.java
@@ -1,56 +1,70 @@
 package hr.redzicleon.library.controllers;
 
-import java.util.UUID;
+import java.util.Set;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import com.querydsl.core.types.Predicate;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import hr.redzicleon.library.domain.Author;
 import hr.redzicleon.library.domain.Book;
-import hr.redzicleon.library.repository.AuthorsRepository;
-import hr.redzicleon.library.repository.BooksRepository;
+import hr.redzicleon.library.domain.dto.book.BookDto;
+import hr.redzicleon.library.domain.dto.book.UpdateBookDto;
+import hr.redzicleon.library.services.BooksService;
 
 @RestController()
 @RequestMapping("books")
 public class BooksController {
-    @Autowired
-    BooksRepository booksRepository;
-    @Autowired
-    AuthorsRepository authorsRepository;
+
+    BooksService booksService;
+
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }
 
     @GetMapping("")
-    public Page<Book> index(@PageableDefault(
-        sort = "createdAt",
-        direction = Sort.Direction.DESC)
-        Pageable pageable,
-        @QuerydslPredicate(root = Book.class) Predicate predicate)
-    {
-        return booksRepository.findAll(predicate, pageable);
+    public Page<Book> index(@PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            @QuerydslPredicate Predicate predicate) {
+        return this.booksService.getBooks(predicate, pageable);
+    }
+
+    @GetMapping("{isbn}")
+    public Book getBook(@PathVariable("isbn") String isbn) {
+        return this.booksService.getBook(isbn);
     }
 
     @PostMapping("")
-    public Book createBook() {
-        Book book = new Book();
-        book.setTitle(UUID.randomUUID().toString());
-        book.setGenre(UUID.randomUUID().toString());
-        book.setISBN("99921-58-10-7");
-        book = booksRepository.save(book);
-        Author author = authorsRepository.findAll(PageRequest.of(0, 5)).getContent().get(0);
-        var list = author.getBooks();
-        list.add(book);
-        author.setBooks(list);
-        authorsRepository.save(author);
-        return book;
+    public Book createBook(@Valid @RequestBody BookDto dto) {
+        return this.booksService.saveBook(dto);
+    }
+
+    @PutMapping("{isbn}")
+    public Book updateBook(@PathVariable("isbn") String isbn, @Valid @RequestBody UpdateBookDto dto) {
+        return this.booksService.updateBook(isbn, dto);
+    }
+
+    @PatchMapping("")
+    public Iterable<Book> updateOrCreateBooks(@RequestBody @NotNull Set<BookDto> dto) {
+        return this.booksService.updateOrCreateBooks(dto);
+    }
+
+    @DeleteMapping("{isbn}")
+    public void deleteBook(@PathVariable("isbn") String isbn) {
+        this.booksService.deleteBook(isbn);
     }
 }
